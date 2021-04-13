f17ba461359a0f0210704cf8f297ed38cb55d859
Emad <almsouti.e@gmail.com>
2021-04-14 00:21:32 +0200

    Updates

---
+++ precache-manifest.04df51a276a69b642787a23df230a6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d212a3ff5b44fed92aea2b2ecd1695",
+    "url": "/Color-Generator/index.html"
+  },
+  {
+    "revision": "1b7f99ee206ead968ce3",
+    "url": "/Color-Generator/static/css/main.cbaf3b31.chunk.css"
+  },
+  {
+    "revision": "0a98153c801a98170165",
+    "url": "/Color-Generator/static/js/2.1d5d43ea.chunk.js"
+  },
+  {
+    "revision": "dba5c495e9fe04b4121db47679137884",
+    "url": "/Color-Generator/static/js/2.1d5d43ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7f99ee206ead968ce3",
+    "url": "/Color-Generator/static/js/main.140aef1c.chunk.js"
+  },
+  {
+    "revision": "08f13fa8a24bafc5c908",
+    "url": "/Color-Generator/static/js/runtime-main.a1d42ae7.js"
+  }
+]);
